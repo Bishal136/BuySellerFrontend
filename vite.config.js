841fa28ead63713459500c46f65c4a6d4aa9c076
Rabbi Hasan Bishal <bishalbiswas2027@gmail.com>
2026-05-05 13:35:12 +0600

    DeploiedV1.0.3.1

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://buysellerbackend-com.onrender.com',
+        target: 'https://buy-seller-backend-com.vercel.app/',
         changeOrigin: true,
       },
     },
