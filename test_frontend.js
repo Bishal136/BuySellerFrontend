e20a068b3287693424f9b6f4239f1a02b4a49551
Rabbi Hasan Bishal <bishalbiswas2027@gmail.com>
2026-05-05 12:40:59 +0600

    Deploied

--- test_frontend.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('/home/bishal/BishalHome/Cours/Ecommarce/Deepshik/App/src/pages/admin/Settings.jsx', 'utf-8');
-if (content.includes('error')) console.log("Has error handlers");
